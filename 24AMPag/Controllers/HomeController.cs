b1639f5c94526449966697ef391d6b69f8bf59e0
MartinArss <mrnitram7u7@gmail.com>
2022-11-03 09:26:04 -0500

    Actualizacion

--- 24AMPag/Controllers/HomeController.cs
+++ 24AMPag/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using _24AMPag.Models;
+﻿using _24AMPag.Context;
+using _24AMPag.Models;
+
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,17 +15,29 @@ namespace _24AMPag.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _context.Usuarios.Include(z => z.Rol).ToListAsync();
+            return View(response);
+        }
+        [HttpGet]
+        public IActionResult Crear()
         {
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
---
+++ 24AMPag/Migrations/20221103130706_ejemplod.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _24AMPag.Migrations
+{
+    public partial class ejemplod : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
